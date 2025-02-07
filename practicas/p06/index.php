dbcf61a05e9dcf850130848ee4fec3ca3f1e969c
anag2603 <gf202326818@alm.buap.mx>
2025-02-06 20:24:56 -0600

    Ejercicio 1 agregado

---
+++ practicas/p06/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 6</title>
+</head>
+<body>
+<h1>Ejercicio 1: Comprobación de múltiplo de 5 y 7</h1>
+
+<p>Introduce un número en la URL como parámetro usando <code>?numero=valor</code>. Por ejemplo: <code>index.php?numero=35</code></p>
+
+<?php
+require_once 'src/funciones.php';
+
+if (isset($_GET['numero'])) {
+    $num = $_GET['numero'];
+
+    if (is_numeric($num)) {
+        if (esMultiploDe5y7($num)) {
+            echo "<h3>El número $num SÍ es múltiplo de 5 y 7.</h3>";
+        } else {
+            echo "<h3>El número $num NO es múltiplo de 5 y 7.</h3>";
+        }
+    } else {
+        echo '<h3>Por favor ingresa un número válido.</h3>';
+    }
+}
+?>
+
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <?php
+        if(isset($_POST["name"]) && isset($_POST["email"]))
+        {
+            echo $_POST["name"];
+            echo '<br>';
+            echo $_POST["email"];
+        }
+    ?>
+</body>
+</html>
---
+++ practicas/p06/src/funciones.php
@@ -0,0 +1,5 @@
+<?php
+function esMultiploDe5y7($numero) {
+    return $numero % 5 === 0 && $numero % 7 === 0;
+}
+?>
