18c0d82b3c1ed7234885de6a2076ef9e49f98f9e
anag2603 <gf202326818@alm.buap.mx>
2025-02-27 22:19:36 -0600

    Ejercicio 3 realizado, archivos modificados

---
+++ practicas/p09/formulario_productos_v2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Producto</title>
+</head>
+<body>
+    <h1>Editar Producto</h1>
+    
+    <?php
+    $producto = null;
+    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+        $id = $_GET['id'];
+        @$link = new mysqli('localhost', 'root', 'Cande02022004', 'marketzone');
+        if ($link->connect_errno) {
+            die('Falló la conexión: ' . $link->connect_error);
+        }
+        if ($stmt = $link->prepare("SELECT * FROM productos WHERE id = ?")) {
+            $stmt->bind_param("i", $id);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $producto = $result->fetch_assoc();
+            $stmt->close();
+        }
+        $link->close();
+    }
+    if (!$producto) {
+        die('<p>Error: Producto no encontrado.</p>');
+    }
+    ?>
+
+    <form id="formularioProductos" action="update_producto.php" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="<?= htmlspecialchars($producto['id']) ?>">
+        <label>Nombre:</label> <input type="text" name="nombre" value="<?= htmlspecialchars($producto['nombre']) ?>" required maxlength="100"><br>
+        <label>Marca:</label> <input type="text" name="marca" value="<?= htmlspecialchars($producto['marca']) ?>" required><br>
+        <label>Modelo:</label> <input type="text" name="modelo" value="<?= htmlspecialchars($producto['modelo']) ?>" required maxlength="25"><br>
+        <label>Precio:</label> <input type="number" step="0.01" name="precio" value="<?= htmlspecialchars($producto['precio']) ?>" required min="100"><br>
+        <label>Unidades:</label> <input type="number" name="unidades" value="<?= htmlspecialchars($producto['unidades']) ?>" required min="0"><br>
+        <label>Detalles:</label> <textarea name="detalles" maxlength="250"><?= htmlspecialchars($producto['detalles']) ?></textarea><br>
+        <label>Imagen:</label> <input type="file" name="imagen"><br>
+        <button type="submit">Actualizar Producto</button>
+    </form>
+</body>
+</html>
---
+++ practicas/p09/get_productos_xhtml_v2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<?php
+    $data = array();
+
+    if(isset($_GET['tope'])) {
+        $tope = $_GET['tope'];
+    } else {
+        die('Parámetro "tope" no detectado...');
+    }
+
+    if (!empty($tope)) {
+        @$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');
+        if ($link->connect_errno) {
+            die('Falló la conexión: '.$link->connect_error.'<br/>');
+        }
+
+        if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope")) {
+            $row = $result->fetch_all(MYSQLI_ASSOC);
+            foreach($row as $num => $registro) {
+                foreach($registro as $key => $value) {
+                    $data[$num][$key] = utf8_encode($value);
+                }
+            }
+            $result->free();
+        }
+        $link->close();
+    }
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Producto</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+    <h3>PRODUCTO</h3>
+    <br/>
+    <?php if(isset($row)) : ?>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Marca</th>
+                    <th scope="col">Modelo</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Unidades</th>
+                    <th scope="col">Detalles</th>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($row as $value) : ?>
+                <tr>
+                    <th scope="row"><?= $value['id'] ?></th>
+                    <td><?= $value['nombre'] ?></td>
+                    <td><?= $value['marca'] ?></td>
+                    <td><?= $value['modelo'] ?></td>
+                    <td><?= $value['precio'] ?></td>
+                    <td><?= $value['unidades'] ?></td>
+                    <td><?= $value['detalles'] ?></td>
+                    <td><img src="<?= $value['imagen'] ?>" width="50"></td>
+                    <td><a href="formulario_productos_v2.php?id=<?= $value['id'] ?>" class="btn btn-primary">Modificar</a></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <script>
+            alert('No hay productos con las condiciones especificadas.');
+        </script>
+    <?php endif; ?>
+</body>
+</html>
