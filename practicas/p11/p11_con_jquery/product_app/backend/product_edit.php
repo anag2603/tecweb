38666a60a0ec4c52aafebcdbb14ee4cdf04018f5
anag2603 <gf202326818@alm.buap.mx>
2025-03-09 23:24:22 -0600

    Trabajando con los archivos para su funcionalidad

--- practicas/p11/p11_con_jquery/product_app/backend/product_edit.php
+++ practicas/p11/p11_con_jquery/product_app/backend/product_edit.php
@@ -1,52 +1,57 @@
 <?php
-    include_once __DIR__.'/database.php';
+include_once __DIR__.'/database.php';
 
-    $producto = file_get_contents('php://input');
-    $data = array(
-        'status'  => 'error',
-        'message' => 'No se encontró el producto para actualizar'
-    );
-    
-    if(!empty($producto)) {
-        $jsonOBJ = json_decode($producto);
-        
+// SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
+$producto = file_get_contents('php://input');
+$data = array(
+    'status'  => 'error',
+    'message' => 'Ya existe un producto con ese nombre'
+);
 
-        $sql = "SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND marca = '{$jsonOBJ->marca}' AND modelo = '{$jsonOBJ->modelo}' AND id != {$jsonOBJ->id} ";
-        $checkResult = $conexion->query($sql);
-        
-        if ($checkResult->num_rows > 0) {
-            $data['message'] = "No se pudo editar, ya existe un producto con ese nombre, marca y modelo";
-        } else {
+if (!empty($producto)) {
+    // SE TRANSFORMA EL STRING DEL JSON A OBJETO
+    $jsonOBJ = json_decode($producto);
 
-            $sql = "SELECT * FROM productos WHERE id = {$jsonOBJ->id} ";
-            $result = $conexion->query($sql);
-            
-            if ($result->num_rows > 0) {
-                // SI EL PRODUCTO EXISTE, SE ACTUALIZA
-                $conexion->set_charset("utf8");
-                $sql = "UPDATE productos SET 
-                            nombre = '{$jsonOBJ->nombre}',
-                            marca = '{$jsonOBJ->marca}', 
-                            modelo = '{$jsonOBJ->modelo}', 
-                            precio = {$jsonOBJ->precio}, 
-                            detalles = '{$jsonOBJ->detalles}', 
-                            unidades = {$jsonOBJ->unidades}, 
-                            imagen = '{$jsonOBJ->imagen}' 
-                        WHERE id = {$jsonOBJ->id} AND eliminado = 0";
-                
-                if ($conexion->query($sql)) {
-                    $data['status'] = "success";
-                    $data['message'] = "Producto actualizado correctamente";
-                } else {
-                    $data['message'] = "ERROR: No se ejecutó $sql. " . mysqli_error($conexion);
-                }
-            }
-            
-            $result->free();
-        }
-        
-        $checkResult->free();
-        $conexion->close();
+    // Verificar si el ID del producto está presente
+    if (!isset($jsonOBJ->id)) {
+        $data['message'] = "El ID del producto es necesario.";
+        echo json_encode($data, JSON_PRETTY_PRINT);
+        exit;
     }
-    echo json_encode($data, JSON_PRETTY_PRINT);
+
+    // Verificar si el nombre del producto ya existe
+    $sql = "SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND id != {$jsonOBJ->id} AND eliminado = 0";
+    $result = $conexion->query($sql);
+
+    if ($result->num_rows == 0) {
+        $conexion->set_charset("utf8");
+
+        // Preparar la consulta para actualizar el producto
+        $sql = "UPDATE productos SET 
+                    nombre = '{$jsonOBJ->nombre}', 
+                    marca = '{$jsonOBJ->marca}', 
+                    modelo = '{$jsonOBJ->modelo}', 
+                    precio = {$jsonOBJ->precio}, 
+                    detalles = '{$jsonOBJ->detalles}', 
+                    unidades = {$jsonOBJ->unidades}, 
+                    imagen = '{$jsonOBJ->imagen}' 
+                WHERE id = {$jsonOBJ->id}";
+
+        if ($conexion->query($sql)) {
+            $data['status'] = "success";
+            $data['message'] = "Producto modificado";
+        } else {
+            $data['message'] = "ERROR: No se ejecutó $sql. " . mysqli_error($conexion);
+        }
+    } else {
+        $data['message'] = "Ya existe un producto con ese nombre.";
+    }
+
+    $result->free();
+    // Cierra la conexión
+    $conexion->close();
+}
+
+// SE HACE LA CONVERSIÓN DE ARRAY A JSON
+echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
