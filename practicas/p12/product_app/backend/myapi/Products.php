d5c15f3964d58c64d4b2dbcd3b97262e4d7ca3ab
Ana Garcia <gf202326818@alm.buap.mx>
2025-04-06 19:24:15 -0600

    Problemas de edicion solucionados

--- practicas/p12/product_app/backend/myapi/Products.php
+++ practicas/p12/product_app/backend/myapi/Products.php
@@ -13,16 +13,14 @@ class Products extends DataBase {
     }
 
     public function add($jsonOBJ) {
-        // SE OBTIENE LA INFORMACIÓN DEL PRODUCTO ENVIADA POR EL CLIENTE
         $this->data = array(
             'status'  => 'error',
             'message' => 'Ya existe un producto con ese nombre'
         );
         if(isset($jsonOBJ->nombre)) {
-            // SE ASUME QUE LOS DATOS YA FUERON VALIDADOS ANTES DE ENVIARSE
             $sql = "SELECT * FROM productos WHERE nombre = '{$jsonOBJ->nombre}' AND eliminado = 0";
             $result = $this->conexion->query($sql);
-            
+
             if ($result->num_rows == 0) {
                 $this->conexion->set_charset("utf8");
                 $sql = "INSERT INTO productos VALUES (null, '{$jsonOBJ->nombre}', '{$jsonOBJ->marca}', '{$jsonOBJ->modelo}', {$jsonOBJ->precio}, '{$jsonOBJ->detalles}', {$jsonOBJ->unidades}, '{$jsonOBJ->imagen}', 0)";
@@ -35,20 +33,16 @@ class Products extends DataBase {
             }
 
             $result->free();
-            // Cierra la conexion
             $this->conexion->close();
         }
     }
 
     public function delete($id) {
-        // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
         $this->data = array(
             'status'  => 'error',
             'message' => 'La consulta falló'
         );
-        // SE VERIFICA HABER RECIBIDO EL ID
         if( isset($id) ) {
-            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
             $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
             if ( $this->conexion->query($sql) ) {
                 $this->data['status'] =  "success";
@@ -61,15 +55,12 @@ class Products extends DataBase {
     }
 
     public function edit($jsonOBJ) {
-        // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
         $this->data = array(
             'status'  => 'error',
             'message' => 'La consulta falló'
         );
-        // SE VERIFICA HABER RECIBIDO EL ID
         if( isset($jsonOBJ->id) ) {
-            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
-            $sql =  "UPDATE productos SET nombre='{$jsonOBJ->nombre}', marca='{$jsonOBJ->marca}',";
+            $sql  = "UPDATE productos SET nombre='{$jsonOBJ->nombre}', marca='{$jsonOBJ->marca}',";
             $sql .= "modelo='{$jsonOBJ->modelo}', precio={$jsonOBJ->precio}, detalles='{$jsonOBJ->detalles}',"; 
             $sql .= "unidades={$jsonOBJ->unidades}, imagen='{$jsonOBJ->imagen}' WHERE id={$jsonOBJ->id}";
             $this->conexion->set_charset("utf8");
@@ -84,13 +75,10 @@ class Products extends DataBase {
     }
 
     public function list() {
-        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
         if ( $result = $this->conexion->query("SELECT * FROM productos WHERE eliminado = 0") ) {
-            // SE OBTIENEN LOS RESULTADOS
             $rows = $result->fetch_all(MYSQLI_ASSOC);
 
             if(!is_null($rows)) {
-                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
                 foreach($rows as $num => $row) {
                     foreach($row as $key => $value) {
                         $this->data[$num][$key] = $value;
@@ -105,16 +93,12 @@ class Products extends DataBase {
     }
 
     public function search($search) {
-        // SE VERIFICA HABER RECIBIDO EL ID
         if( isset($search) ) {
-            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
             $sql = "SELECT * FROM productos WHERE (id = '{$search}' OR nombre LIKE '%{$search}%' OR marca LIKE '%{$search}%' OR detalles LIKE '%{$search}%') AND eliminado = 0";
             if ( $result = $this->conexion->query($sql) ) {
-                // SE OBTIENEN LOS RESULTADOS
                 $rows = $result->fetch_all(MYSQLI_ASSOC);
 
                 if(!is_null($rows)) {
-                    // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
                     foreach($rows as $num => $row) {
                         foreach($row as $key => $value) {
                             $this->data[$num][$key] = $value;
@@ -131,13 +115,10 @@ class Products extends DataBase {
 
     public function single($id) {
         if( isset($id) ) {
-            // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
             if ( $result = $this->conexion->query("SELECT * FROM productos WHERE id = {$id}") ) {
-                // SE OBTIENEN LOS RESULTADOS
                 $row = $result->fetch_assoc();
-    
+
                 if(!is_null($row)) {
-                    // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
                     foreach($row as $key => $value) {
                         $this->data[$key] = $value;
                     }
@@ -151,10 +132,6 @@ class Products extends DataBase {
     }
 
     public function getData() {
-        // SE HACE LA CONVERSIÓN DE ARRAY A JSON
         return json_encode($this->data, JSON_PRETTY_PRINT);
     }
 }
-
-//$productos = new Productos();
-?>
--- practicas/p12/product_app/backend/product-edit.php
+++ practicas/p12/product_app/backend/product-edit.php
@@ -1,6 +1,6 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
 use TECWEB\MYAPI\Products;
-require_once __DIR__.'/myapi/Products.php';
 
 $productos = new Products('marketzone', 'root', 'Cande02022004');
 $productos->edit(json_decode(json_encode($_POST)));
